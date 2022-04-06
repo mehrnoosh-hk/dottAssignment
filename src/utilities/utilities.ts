83cd3ded83a9c0a124c97217fd3888f085cc36bf
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2022-04-06 15:16:16 +0430

    Refactor utilities

--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -1,69 +1,88 @@
 import * as readline from 'readline';
 import * as fs from 'fs';
 
-
-// TODO: Move error definitions to a file
-
 /**
- * Create a readline interface for the user to interact with.
- * @param {string} filePath The address to the file to read from.
- * @return {Promise<readline.ReadLine>} A readline interface.
+ * A class that provides nessessary utility functions to work with test files.
  */
-export function CreateReadlineInterface(filePath: string):
-                                        Promise<readline.ReadLine> {
-  return new Promise((resolve, reject) => {
-    try {
-      const rl = readline.createInterface({
-        input: fs.createReadStream(filePath),
+export class Utils {
+
+  public filePath: string;
+  public rl: readline.ReadLine[];
+
+  constructor(filePath: string){
+    this.filePath = filePath;
+    this.rl = [];
+    this.createReadlineInterface();
+  }
+
+  /**
+   * Create a readline interface for the user to interact with.
+   * If an interface already exists, return it.
+   * @return {Promise<readline.ReadLine>} A readline interface.
+   */
+  createReadlineInterface():Promise<readline.ReadLine> {
+    if (this.rl.length > 0) {
+      return Promise.resolve(this.rl[0]);
+    } else {
+
+      return new Promise((resolve, reject) => {
+        try {
+          const rl = readline.createInterface({
+          input: fs.createReadStream(this.filePath),
+          });
+          this.rl.push(rl)
+          resolve(rl);
+        } catch (error) {
+          reject(error);
+        }
       });
-      resolve(rl);
-    } catch (error) {
-      reject(error);
-    }
-  });
-}
+    }  
+  }
+
+  /**
+   * Close any open readline interface
+   */
+  closeReadlineInterface() {
+    this.rl[0].close();
+  }
+
+  /**
+   * Reads the nth line of a file.
+   * @param {number} n The line number to read.
+   * @return {Promise<string>} A promise that resolves to the 
+   * nth line of the file.
+   */
+  readNthLine(n: number):Promise<string> {
+    return new Promise((resolve, reject) => {
+      let lineNumber = 0;
+      this.rl[0].on('line', (line) => {
+        lineNumber++;
+        if (lineNumber === n) {
+          resolve(line);
+        }
+      });
+
+      this.rl[0].on('error', reject);
+    });
+  }
 
 
-/**
- * Reads the nth line of a file.
- * @param {readline.ReadLine} rl The readline interface to read from.
- * @param {number} n The line number to read.
- * @return {Promise<string>} A promise that resolves to the 
- * nth line of the file.
- */
-export function ReadNthLine(rl: readline.Interface, n: number):
-                            Promise<string> {
-  return new Promise((resolve, reject) => {
-    let lineNumber = 0;
-    rl.on('line', (line) => {
-      lineNumber++;
-      if (lineNumber === n) {
-        rl.close();
-        // TODO: Improve to close only once
-        resolve(line);
+  /**
+   * Reads the number of problems from test file.
+   * @return {Promise<number>} A promise that resolves to the number of problems.
+   */
+  ReadNumberOfProblems(): Promise<number> {
+    return new Promise((resolve, reject) => {
+      try {
+          this.readNthLine(1).then((line) => {
+            resolve(parseInt(line));
+          });
+        } catch (error) {
+        reject(error);
       }
     });
+  };
 
-    rl.on('error', reject);
-  });
 }
 
-/**
- * Reads the number of problems from test file.
- * @param {string} filePath The address to the file to read from.
- * @return {Promise<number>} A promise that resolves to the number of problems.
- */
-export async function ReadNumberOfProblems(filePath: string): Promise<number> {
-  return new Promise((resolve, reject) => {
-    try {
-      CreateReadlineInterface(filePath).then((rl) => {
-        const lineNumber = 1;
-        ReadNthLine(rl, lineNumber).then((line) => {
-          resolve(parseInt(line));
-        });
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
+// TODO: Move error definitions to a file
--- test/utilities/utilities.test.ts
+++ test/utilities/utilities.test.ts
@@ -1,62 +1,42 @@
-import {CreateReadlineInterface, ReadNthLine,
-  ReadNumberOfProblems} from '../../src/utilities/utilities';
+import {Utils} from '../../src/utilities/utilities';
+import * as readline from 'readline';
 
-
-const filePath = 'test/utilities/mockFile.txt';
 
 // Test for create readline interface
-describe('Create ReadLine Interface', () => {
+describe('Test implementation of Utils class', () => {
+  const util = new Utils('test/utilities/mockFile.txt');
+
   test('Create a readline interface', () => {
-    CreateReadlineInterface(filePath).then((rl) => {
-      expect(rl).toBeDefined();
-      rl.close();
-    });
+    expect(util.rl).toBeDefined(); 
   });
 
-  // TODO: Add test for invalid file path
+  test('There is only one instance of readline interface', () => {
+    expect(util.rl.length).toBe(1);
+  });
+
+  test('The rl property is an instance of readline interface', () => {
+    expect(util.rl[0]).toBeInstanceOf(readline.Interface);
+  });
+
+  test('Read first line of file', () => {
+    expect(util.readNthLine(1)).resolves.toBe('2');
+  });
+
+  test('Read the third line of file', () => {
+    expect(util.readNthLine(3)).resolves.toBe('1101');
+  })
+
+  test('Read the number of problems', () => {
+    expect(util.ReadNumberOfProblems()).resolves.toBe(2);
+  })
+
+  
 });
+
+// TODO: Add test for invalid file path
+// TODO: Add test for invalid line number
+// TODO: Add test for close readline interface
 
 
 // TODO: A worker process has failed to exit gracefully and has been force exited. This is likely caused by tests leaking due to improper teardown. Try running with --detectOpenHandles to find leaks. Active timers can also cause this, ensure that .unref() was called on them.
 
-
-// Test the read nth line of a file
-describe('Read the nth line of a txt file', () => {
-  test('Read the first line of a file', async () => {
-    const n = 1;
-    CreateReadlineInterface(filePath).then((rl) => {
-      ReadNthLine(rl, n).then((line) => {
-        expect(line).toBe('2');
-        rl.close();
-      });
-    });
-  });
-
-  test('Read Third line of file', () => {
-    const n = 3;
-    CreateReadlineInterface(filePath).then((rl) => {
-      ReadNthLine(rl, n).then((line) => {
-        expect(line).toBe('1101');
-        rl.close();
-      });
-    });
-  });
-
-  test('Read a line that does not exist', () => {
-    const n = 100;
-    CreateReadlineInterface(filePath).then((rl) => {
-      ReadNthLine(rl, n).then((line) => {
-        expect(line).toBe('');
-        rl.close();
-      });
-    });
-  });
-});
-
-
-describe('Read the number of problems in a file', () => {
-  test('Read the number of problems in a file', async () => {
-    const numberOfProblems = await ReadNumberOfProblems(filePath);
-    expect(numberOfProblems).toBe(2);
-  });
-});
